75f7594f9026fc7c547c01d81cad8ceeaf8d23dd
philodia <tawethiat@gmail.com>
2024-11-11 18:24:40 +0000

    modifier db.js et .env

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,5 @@
-const mongoose =require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect(
-    process.env.MONGO_URI, {
-        UseNewUrlParser :true ,
-        UseUnifiedTopology: true 
-
-    }
-)
-.then(()=> console.log("successfuly connection to database"))
-.catch((err)=>console.log(err))
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("Successfully connected to the database"))
+    .catch((err) => console.log(err));
